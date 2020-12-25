9ff821906807a4045776719a99129adb6a191c33
kaixa <690750353@qq.com>
2020-12-26 00:14:33 +0800

    lab3

---
+++ lab3/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <my_math.h>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    auto c = add(19, 19);
+    cout << "lab3, result is: " << c << endl;
+    return 0;
+}
