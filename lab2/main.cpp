4bda4041334b7d51f893fb39d5d5aa432db2dfae
kaixa <690750353@qq.com>
2020-12-25 23:58:01 +0800

    lab2

---
+++ lab2/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "math.h"
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    auto c = add(114, 514);
+    cout << "result is: " << c << endl;
+    return 0;
+}
